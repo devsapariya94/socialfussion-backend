52c704e73ac1c5f8445c57257076d98f83a30888
Dev <devsapariya94@gmail.com>
2023-11-11 15:29:26 +0530

    update the app.py

--- app.py
+++ app.py
@@ -3,18 +3,17 @@ from flask_cors import CORS
 from flask_login import LoginManager
 from bson import ObjectId
 
-if __name__ == '__main__':
-    from insta_routes import instroute
-    from yt_routes import ytroute
-    from auth import authRoute
-    from protected_route import protectedRoute
-    app = Flask(__name__)
-    login_manager = LoginManager(app)
-    login_manager.login_view = 'authRoute.login'
-    CORS(app, origins="*")
-    app.register_blueprint(instroute)
-    app.register_blueprint(ytroute)
-    app.register_blueprint(authRoute)
-    app.register_blueprint(protectedRoute)
-    app.run(debug=True)
+from insta_routes import instroute
+from yt_routes import ytroute
+from auth import authRoute
+from protected_route import protectedRoute
+app = Flask(__name__)
+login_manager = LoginManager(app)
+login_manager.login_view = 'authRoute.login'
+CORS(app, origins="*")
+app.register_blueprint(instroute)
+app.register_blueprint(ytroute)
+app.register_blueprint(authRoute)
+app.register_blueprint(protectedRoute)
+app.run(debug=True)
 
