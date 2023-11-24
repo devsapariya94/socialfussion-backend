6c7846855aa163bb7d054d900a20075949fda2a0
Dev <devsapariya94@gmail.com>
2023-11-24 09:14:21 +0530

    testing

--- protected_route.py
+++ protected_route.py
@@ -121,12 +121,13 @@ def hello():
 @token_required
 def insta_following(current_user):
         request_data = request.get_json()
+        print(request_data)
         username = request_data['username']
     # try:
 
         header = {"User-Agent": "Instagram 219.0.0.12.117 Android"}
         response = requests.get(f"https://www.instagram.com/api/v1/users/web_profile_info/?username={username}", headers=header)
-
+        print(response)
         print(response.json()["data"]["user"]["is_private"])
         with open("data.json", "w") as outfile:
             json.dump(response.json(), outfile)
